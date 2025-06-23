1d43bf245eeac14091000543464a17dedeee0ccd
MohnishKumarS <mohnish101998@gmail.com>
2025-06-23 18:24:02 +0530

    app store design changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 
-window.onload = function () {
-    // Delay of 1 second before hiding the preloader
-    setTimeout(function () {
-        // Hide the preloader
-        document.getElementById('preloader').style.display = 'none';
+// window.onload = function () {
+//     // Delay of 1 second before hiding the preloader
+//     setTimeout(function () {
+//         // Hide the preloader
+//         document.getElementById('preloader').style.display = 'none';
 
-    }, 1000);
-};
+//     }, 1000);
+// };
 
 // changing product Qty 
 function changeQuantity(element,step) {
